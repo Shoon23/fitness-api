169db59a9cb85a81c73eeb1859b1bebc3c206df0
sean <custodiocsean@gmail.com>
2024-03-17 20:10:51 +0800

    handle register routes

--- src/controller/auth_controller.ts
+++ src/controller/auth_controller.ts
@@ -2,13 +2,29 @@ import { Request, Response } from "express";
 import prisma from "../lib/prisma";
 import bcrypt from "bcrypt";
 import generate_jwt from "../utils/generate_jwt";
+import Joi from "joi";
+import { abort } from "process";
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]")).required(),
+});
+
+const registerSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]")).required(),
+});
+
 export default {
   async login(req: Request, res: Response) {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ error: "Missing Email or Password" });
+    const { value, error } = loginSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
     }
 
+    const { email, password } = value;
     try {
       const user = await prisma.user.findUnique({
         where: {
@@ -37,15 +53,67 @@ export default {
         });
       }
 
-      const access_token = generate_jwt("123");
+      const access_token = generate_jwt(user.id);
       const { password: hash_password, ...other } = user;
-      res.status(201).json({
+
+      res.status(200).json({
         ...other,
         access_token,
+        name,
       });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ error: "Something Went Wrong" });
     }
   },
-  register(req: Request, res: Response) {},
+  async register(req: Request, res: Response) {
+    const { value, error } = registerSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { first_name, last_name, email, password } = value;
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        return res.status(409).json({
+          message: "User Already Exists",
+        });
+      }
+
+      const hash_password = await bcrypt.hash(password, 10);
+
+      const create_user = await prisma.user.create({
+        data: {
+          first_name,
+          last_name,
+          email,
+          password: hash_password,
+        },
+        select: {
+          id: true,
+          first_name: true,
+          last_name: true,
+          email: true,
+        },
+      });
+
+      const access_token = generate_jwt(create_user.id);
+      return res.status(201).json({
+        ...create_user,
+        access_token,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "Something Went Wrong",
+      });
+    }
+  },
 };
--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import auth_controller from "../controller/auth_controller";
 const auth_routes = Router();
 
-auth_routes.get("/login", auth_controller.login);
-auth_routes.post("/register");
+auth_routes.post("/login", auth_controller.login);
+auth_routes.post("/register", auth_controller.register);
 
 export default auth_routes;
