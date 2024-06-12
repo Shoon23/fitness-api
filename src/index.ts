d10e4757d11d683bbd77d84e629ada141f9d2a6b
sean <custodiocsean@gmail.com>
2024-06-12 15:14:21 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use(express.static("public/exercises_data/exercises_img"));
 app.use(express.json());
